523eb0386c4f8bc79d5cdb9bbbba9e9c58e7da7d
katrod <katrod4545@gmail.com>
2023-06-01 23:18:53 -0600

    deleted mislabeled assets folder developed js code edited css file edited index edited readme to be merged upon commit

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,71 +1,51 @@
-// displays current date and time - done
-$(document).ready(function(){
-const date = new Date();
-console.log(date); 
-let currentDates = document.getElementById("currentDay");
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-currentDates.innerHTML = date; 
-let currentDate = `${month}.${day}.${year}`;
-console.log(currentDate); 
-});
+// todays date using dayjs
+var todayDate = dayjs().format('dddd, MMM Do YYYY');
+console.log(todayDate)
+$("#currentDay").html(todayDate);
+// click listener
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+      var textarea = $(this).siblings(".description").val();
+      console.log(textarea)
+      var timeslot = $(this).parent().attr("id");
+      console.log(timeslot)
+      localStorage.setItem(timeslot, textarea);
+  })
 
-// save switch toggle - 
-var toggleSave = document.querySelector('#color-change');
-var savbtn = document.querySelectorAll("button");
-var btnmode = "black";
 
-toggleSave.addEventListener("click", function() {
-   
-    if (btnmode === "black") {
-btnmode = "white";
-savbtn.setAttribute("class" , "white")
+function itsTime() {
+  var timeDiv = dayjs().hour();
+  console.log(timeDiv)
 
-}else{
-btnmode = "black";
-savbtn.setAttribute("class" , "black")
+// changes block color dependent on current time
+$(".time-block").each(function () {
+  var todayDay = parseInt($(this).attr("id").split("hour")[1]);
+  if (todayDay === timeDiv) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  } else if (todayDay < timeDiv) {
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  } else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+})
+
 }
-});
 
-// saves schedule info into local storage
-var scheduleArea = document.querySelectorAll("textarea");
+$("#hr-9 .description").val(localStorage.getItem("hr-9"));
+$("#hr-10 .description").val(localStorage.getItem("hr-10"));
+$("#hr-11 .description").val(localStorage.getItem("hr-11"));
+$("#hr-12 .description").val(localStorage.getItem("hr-12"));
+$("#hr-13 .description").val(localStorage.getItem("hr-13"));
+$("#hr-14 .description").val(localStorage.getItem("hr-14"));
+$("#hr-15 .description").val(localStorage.getItem("hr-15"));
+$("#hr-16 .description").val(localStorage.getItem("hr-16"));
+$("#hr-17 .description").val(localStorage.getItem("hr-17"));
 
-
-toggleSave.addEventListener("click", function() {
-    localStorage.setItem("scheduleArea", JSON.stringify(scheduleArea));
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// color coding for time blocks depending on time of day
-let currentDay = dayjs().format("HH")
-
-$(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
-    
-    if (currentDay == timeDiv) {
-      $(this).addClass(".present");
-      $(this).children(".description").addClass(".pink");
-    } else if (currentDay < timeDiv) {
-      $(this).removeClass(".present");
-      $(this).addClass(".future");
-    } else if (currentDay > timeDiv) {
-      $(this).removeClass(".future");
-      $(this).addClass(".past");
-    }
-  });
-
-
-
-
+itsTime();
+})
